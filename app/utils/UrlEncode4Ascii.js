fc0e3c724e97e4a1eca24607710612130d4b2470
dll <kaixin890312@163.com>
2015-10-17 21:38:11 +0800

    add urlDecode for ascii

--- app/utils/UrlEncode4Ascii.js
+++ app/utils/UrlEncode4Ascii.js
@@ -64,6 +64,33 @@ function urlEncode(str) {
     return ret;
 }
 
+function urlDecode(str) {
+    var ret = "";
+    for (var i = 0; i < str.length; i++) {
+        var chr = str.charAt(i);
+        if (chr == "+") {
+            ret += " ";
+        }
+        else if (chr == "%") {
+            var asc = str.substring(i + 1, i + 3);
+            if (parseInt("0x" + asc) > 0x7f) {
+                ret += asc2str(parseInt("0x" + asc + str.substring(i + 4, i + 6)));
+                i += 5;
+            }
+            else {
+                ret += asc2str(parseInt("0x" + asc));
+                i += 2;
+            }
+        }
+        else {
+            ret += chr;
+        }
+    }
+
+    return ret;
+}
+
 module.exports = {
-    urlEncode4Ascii: urlEncode
+    urlEncode4Ascii: urlEncode,
+    urlDecode4Ascii: urlDecode
 }
